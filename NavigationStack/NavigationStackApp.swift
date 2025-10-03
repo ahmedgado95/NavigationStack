6c7822349a989445042fd645e09e5a4f5d01f2ee
AhmedGado <ahmedgado2001@gmail.com>
2025-10-03 18:24:36 +0300

    Add app structure

---
+++ NavigationStack/NavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackApp.swift
+//  NavigationStack
+//
+//  Created by ahmed gado on 22/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+    
+        }
+    }
+}
---
+++ NavigationStackTests/NavigationStackTests.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackTests.swift
+//  NavigationStackTests
+//
+//  Created by ahmed gado on 22/09/2025.
+//
+
+import Testing
+@testable import NavigationStack
+
+struct NavigationStackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
